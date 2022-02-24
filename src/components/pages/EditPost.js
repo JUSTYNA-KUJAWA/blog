8a1bddde7baf7fee7223de92e85883904c47401d
Justyna KUJAWA <justy.kujawa@gmail.com>
2022-02-24 21:02:36 +0100

    19.7

--- src/components/pages/EditPost.js
+++ src/components/pages/EditPost.js
@@ -1,6 +1,11 @@
+import EditPostForm from "../features/EditPostForm";
+
 const EditPost = () => {
     return (
+      <div>
       <h2>Edit Post</h2>
+      <EditPostForm/>
+    </div>
     );
   }
   
